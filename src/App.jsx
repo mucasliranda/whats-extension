8ba6d89e0c3968173b8b41a07c3841f7d3a2d20a
Lucas Miranda <93141172+mucasliranda@users.noreply.github.com>
2023-08-10 09:12:22 -0300

    feat: aplicação finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,51 @@
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { useCallback, useState } from 'react'
 
+export default function App() {
+  const [telefone, setTelefone] = useState()
 
+  const onSubmit = useCallback((e) => {
+    e.preventDefault()
+    window.open(`https://wa.me/${telefone}`)
+  }, [telefone])
 
-function App() {
   return (
-    <main className="w-full bg-green-500 flex flex-col justify-center items-center">
-      <h3>
-        Abrir conversa
-      </h3>
-
-      <form className="w-[calc(100% - 2rem)]">  
-        <input placeholder="aaaaa" />
-
-        <button
-          type="submit"
-          className="
-            font-bold py-2 px-4 rounded
-            bg-blue-500 text-white
-            hover:bg-blue-700
-          "
+    <main className="w-[24rem] h-[24rem] bg-green-500 flex flex-col justify-center items-center">
+      <div className="w-[calc(100%-3rem)] h-[calc(100%-3rem)] bg-red-400 flex flex-col justify-center items-center
+        rounded-md bg-clip-padding backdrop-filter backdrop-blur-xl bg-opacity-10 border border-gray-100
+      ">
+        <form 
+          className="flex flex-col justify-center items-stretch gap-6 text-black"
+          onSubmit={onSubmit}
         >
+          <h1 
+            className="text-lg text-center font-medium text-white"
+          >
+            Abrir conversa
+          </h1>
 
-        </button>
-      </form>
+          <div className='border border-gray-100 pl-2'>
+            <PhoneInput 
+            defaultCountry='BR'
+              placeholder="Digite o número aqui"
+              value={telefone}
+              onChange={setTelefone}
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="
+              font-bold py-1 px-2 rounded
+              bg-blue-500 text-white 
+              hover:bg-blue-700
+            "
+          >
+            Enviar
+          </button>
+        </form>
+      </div>
     </main>
   )
 }
-
-export default App
